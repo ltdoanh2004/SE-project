78d187f8fbf8e60ba4dd1334491780a5583ee24f
“hwHoai” <daohuuhoai2655@email.com>
2025-04-30 23:20:21 +0700

    Hoai | fix merge

--- src/components/provider/provider.jsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export const AuthProvider = createContext({isAuth: false, setIsAuth: () => {}});
-export const UserPublicInfoProvider = createContext({userInfo: {userId: 0, userName: ''}, setUserInfo: () => {}});
